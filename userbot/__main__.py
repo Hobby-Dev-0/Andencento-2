8fdbabab9ef919601628ac7ce1d4e3f97684dd6d
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2022-02-28 17:13:26 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -83,12 +83,13 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 
+"""
 async def op():
     await Andencento(JoinChannelRequest("Andencento"))
     await Andencento(JoinChannelRequest("AndencentoSupport"))
         
 Andencento.loop.create_task(op())
-
+"""
 print("Andencento Deployed And Working Fine")
 
 if len(sys.argv) not in (1, 3, 4):
